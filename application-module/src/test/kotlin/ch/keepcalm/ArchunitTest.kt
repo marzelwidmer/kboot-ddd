aa7b86fda127094c3d81899289c2ade6b9956178
c3smonkey <c3smonkey@gmail.com>
2023-10-27 16:12:55 +0200

    test: add ArchUnit

---
+++ application-module/src/test/kotlin/ch/keepcalm/ArchunitTest.kt
@@ -0,0 +1,22 @@
+package ch.keepcalm
+
+import com.tngtech.archunit.core.domain.JavaClasses
+import com.tngtech.archunit.core.importer.ClassFileImporter
+import com.tngtech.archunit.lang.ArchRule
+import com.tngtech.archunit.lang.syntax.ArchRuleDefinition.noClasses
+import org.junit.jupiter.api.Test
+
+class ArchitectureTest {
+
+    @Test
+    fun `validate architecture`() {
+        val importedClasses: JavaClasses = ClassFileImporter().importPackages("ch.keepcalm")
+
+        val rule: ArchRule = noClasses()
+            .that().resideInAPackage("..domain..")
+            .should().dependOnClassesThat()
+            .resideInAnyPackage("..application..", "..infrastructure..")
+
+        rule.check(importedClasses)
+    }
+}
