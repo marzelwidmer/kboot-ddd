a9e4d1d84648ff51f99f9eb9fedcc8a05781d8c0
c3smonkey <c3smonkey@gmail.com>
2023-10-29 22:01:20 +0100

    test: add StringSpec Kotest to the Service

---
+++ application-module/src/test/kotlin/ch/keepcalm/application/partner/PartnerServiceTests.kt
@@ -0,0 +1,42 @@
+package ch.keepcalm.application.partner
+
+import ch.keepcalm.ddd.partner.Partner
+import ch.keepcalm.ddd.partner.PartnerId
+import ch.keepcalm.ddd.partner.PartnerName
+import ch.keepcalm.domain.partner.PartnerRepository
+import io.kotest.core.spec.style.StringSpec
+import io.kotest.matchers.shouldBe
+import io.mockk.every
+import io.mockk.mockk
+import io.mockk.verify
+
+class PartnerServiceTest : StringSpec({
+
+    val mockPartnerRepository = mockk<PartnerRepository>()
+    val partnerService = PartnerService(mockPartnerRepository)
+
+    "createPartner should save a partner" {
+        val name = "Test Partner"
+
+        every { mockPartnerRepository.save(any()) } returns Unit
+
+        partnerService.createPartner(name)
+
+        // Verify that save was called on mockPartnerRepository with any Partner object
+        verify { mockPartnerRepository.save(any()) }
+    }
+
+    "getPartnerById should return a partner" {
+        val id = "test-id"
+        val expectedPartner = Partner(PartnerId(id), PartnerName("Test"))
+
+        every { mockPartnerRepository.findById(id) } returns expectedPartner
+
+        val result = partnerService.getPartnerById(id)
+
+        result shouldBe expectedPartner
+
+        // Verify that findById was called on mockPartnerRepository with the correct id
+        verify { mockPartnerRepository.findById(id) }
+    }
+})
