22d551e5635ec5a5b29cdaed95eb1194a1027df5
c3smonkey <c3smonkey@gmail.com>
2023-10-29 21:04:55 +0100

    chore: add gitlog maven plugin

--- application-module/src/main/kotlin/ch/keepcalm/infrastructure/partner/redis/PartnerDTO.kt
+++ application-module/src/main/kotlin/ch/keepcalm/infrastructure/partner/redis/PartnerDTO.kt
@@ -4,6 +4,5 @@ import org.springframework.data.annotation.Id
 import org.springframework.data.redis.core.RedisHash
 import org.springframework.data.redis.core.index.Indexed
 
-// DTO
 @RedisHash("partner")
 class PartnerDTO(@Id val id: String, @Indexed val name: String)
