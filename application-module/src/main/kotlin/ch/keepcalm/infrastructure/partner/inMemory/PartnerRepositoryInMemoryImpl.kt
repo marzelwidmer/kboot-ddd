bec5cac244beba080476bf19313688bda32b96cd
c3smonkey <c3smonkey@gmail.com>
2023-10-29 21:38:45 +0100

    chore: add inMemory Spring profile

--- application-module/src/main/kotlin/ch/keepcalm/infrastructure/partner/inMemory/PartnerRepositoryInMemoryImpl.kt
+++ application-module/src/main/kotlin/ch/keepcalm/infrastructure/partner/inMemory/PartnerRepositoryInMemoryImpl.kt
@@ -2,8 +2,10 @@ package ch.keepcalm.infrastructure.partner.inMemory
 
 import ch.keepcalm.ddd.partner.Partner
 import ch.keepcalm.domain.partner.PartnerRepository
+import org.springframework.context.annotation.Profile
 import org.springframework.stereotype.Repository
 
+@Profile("inMemory")
 @Repository
 class PartnerRepositoryInMemoryImpl : PartnerRepository {
     private val inMemoryStorage = mutableMapOf<String, Partner>()
