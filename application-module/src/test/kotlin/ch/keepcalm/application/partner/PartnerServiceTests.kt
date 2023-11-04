9de146a5b1d6855acc2a2b63d68f7f0d0e8baf08
c3smonkey <c3smonkey@gmail.com>
2023-11-04 18:06:25 +0100

    refactor: refactor parnter service class

--- application-module/src/test/kotlin/ch/keepcalm/application/partner/PartnerServiceTests.kt
+++ application-module/src/test/kotlin/ch/keepcalm/application/partner/PartnerServiceTests.kt
@@ -9,34 +9,34 @@ import io.kotest.matchers.shouldBe
 import io.mockk.every
 import io.mockk.mockk
 import io.mockk.verify
-//
-//class PartnerServiceTest : StringSpec({
-//
-//    val mockPartnerRepository = mockk<PartnerRepository>()
-//    val partnerService = PartnerService(mockPartnerRepository)
-//
-//    "createPartner should save a partner" {
-//        val name = "Test Partner"
-//
-//        every { mockPartnerRepository.save(any()) } returns Unit
-//
-//        partnerService.createPartner(name)
-//
-//        // Verify that save was called on mockPartnerRepository with any Partner object
-//        verify { mockPartnerRepository.save(any()) }
-//    }
-//
-//    "getPartnerById should return a partner" {
-//        val id = "test-id"
-//        val expectedPartner = Partner(PartnerId(id), PartnerName("Test"))
-//
-//        every { mockPartnerRepository.findById(id) } returns expectedPartner
-//
-//        val result = partnerService.getPartnerById(id)
-//
-//        result shouldBe expectedPartner
-//
-//        // Verify that findById was called on mockPartnerRepository with the correct id
-//        verify { mockPartnerRepository.findById(id) }
-//    }
-//})
+
+class PartnerServiceTest : StringSpec({
+
+    val mockPartnerRepository = mockk<PartnerRepository>()
+    val partnerService = PartnerServiceImpl(mockPartnerRepository)
+
+    "createPartner should save a partner" {
+        val name = "Test Partner"
+
+        every { mockPartnerRepository.save(any()) } returns Unit
+
+        partnerService.createPartner(name)
+
+        // Verify that save was called on mockPartnerRepository with any Partner object
+        verify { mockPartnerRepository.save(any()) }
+    }
+
+    "getPartnerById should return a partner" {
+        val id = "test-id"
+        val expectedPartner = Partner(PartnerId(id), PartnerName("Test"))
+
+        every { mockPartnerRepository.findById(id) } returns expectedPartner
+
+        val result = partnerService.getPartnerById(id)
+
+        result shouldBe expectedPartner
+
+        // Verify that findById was called on mockPartnerRepository with the correct id
+        verify { mockPartnerRepository.findById(id) }
+    }
+})
